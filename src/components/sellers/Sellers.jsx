45da882fd5ad0f2268426b836bd1e5fe130a7ded
KumudhaLakshmikanth <kumudhal02@gmail.com>
2023-11-23 13:43:46 +0530

    all ecommerce files

--- src/components/sellers/Sellers.jsx
+++ src/components/sellers/Sellers.jsx
@@ -3,7 +3,7 @@ import './sellers.css'
 import { useEffect } from 'react';
 import Slider from 'react-slick';
 // import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel/slick/slick-theme.css';
 
 const SellersData=[
     {
